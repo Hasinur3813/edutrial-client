eaefd71c04180c524e70945160339319a995c62c
Hasinur <hasinur.rahman1332@gmail.com>
2025-01-22 21:59:30 +0600

    added api-url

--- src/axios/useAxiosPublic.jsx
+++ src/axios/useAxiosPublic.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosPublic = axios.create({
-  // baseURL: "https://edutrial.vercel.app",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://edutrial.vercel.app",
+  // baseURL: "http://localhost:3000",
   withCredentials: true,
 });
 
